415f2ab35a1339a749864dd3352b18d8a86162ff
Aria Moraine <ariamoraine@gmail.com>
2012-07-28 11:52:12 -0400

    start up git hub

---
+++ gameclient.py
@@ -0,0 +1,18 @@
+import socket
+import threading
+import pickle
+
+newList = ['this', 'is', 'a', 'list', 'sent', 'from', 'the', 'client']
+pickledList = pickle.dumps(newList)
+
+class ConnectionThread(threading.Thread):
+
+    def run(self):
+        client = socket.socket ()
+        client.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)        
+        client.connect (('10.242.11.167', 2727))
+        print pickle.loads (client.recv(1024))
+        client.send(pickledList)
+        client.close()
+
+ConnectionThread().start()
---
+++ gameserver.py
@@ -0,0 +1,34 @@
+import sys
+import threading
+import socket
+import pickle
+import os
+
+myList = ["i'm", "a", "list", "sent", "from", "the", "server"]
+pickledList = pickle.dumps (myList)
+
+class ClientThread (threading.Thread):
+
+    def __init__ (self, SocketObject, IpAddress):
+        self.SocketObject = SocketObject
+        self.IpAddress = IpAddress
+        threading.Thread.__init__(self)
+
+    def run(self):
+        print 'Started a connection with:', self.SocketObject, 'At:', IpAddress
+        self.SocketObject.send (pickledList)
+        print pickle.loads (self.SocketObject.recv(1024))
+        self.SocketObject.close()
+        print 'Closed the connection with:', SocketObject, 'At:', IpAddress
+
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+server.bind (('10.242.11.167', 2727))
+server.listen (3)
+
+try:
+    while True:
+        SocketObject, IpAddress = server.accept()
+        ClientThread (SocketObject, IpAddress).start()
+except KeyboardInterrupt:
+    os._exit(0)
